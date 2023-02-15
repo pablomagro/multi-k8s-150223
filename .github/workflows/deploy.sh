a21f600750c0c81c4906ed477f507e2d1296bc8a
Pablo Magro <pmagas@gmail.com>
2023-02-15 23:46:24 +1300

    Fix misc issue(s) v17

--- .github/workflows/deploy.sh
+++ .github/workflows/deploy.sh
@@ -1,16 +1,35 @@
-docker build -t pmagas/multi-client-k8s:latest -t pmagas/multi-client-k8s:$GITHUB_SHA -f ./client/Dockerfile ./client
-docker build -t pmagas/multi-server-k8s-pgfix:latest -t pmagas/multi-server-k8s-pgfix:$GITHUB_SHA -f ./server/Dockerfile ./server
-docker build -t pmagas/multi-worker-k8s:latest -t pmagas/multi-worker-k8s:$GITHUB_SHA -f ./worker/Dockerfile ./worker
+#!/bin/bash
 
-docker push pmagas/multi-client-k8s:latest
-docker push pmagas/multi-server-k8s-pgfix:latest
-docker push pmagas/multi-worker-k8s:latest
+echo "======================================="
+echo " Building docket images!"
+echo "======================================="
 
-# docker push pmagas/multi-client-k8s:$GITHUB_SHA
-# docker push pmagas/multi-server-k8s-pgfix:$GITHUB_SHA
-# docker push pmagas/multi-worker-k8s:$GITHUB_SHA
+docker build -t stephengrider/multi-client-k8s:latest -t stephengrider/multi-client-k8s:$GITHUB_SHA -f ./client/Dockerfile ./client
+docker build -t stephengrider/multi-server-k8s-pgfix:latest -t stephengrider/multi-server-k8s-pgfix:$GITHUB_SHA -f ./server/Dockerfile ./server
+docker build -t stephengrider/multi-worker-k8s:latest -t stephengrider/multi-worker-k8s:$GITHUB_SHA -f ./worker/Dockerfile ./worker
+
+echo "======================================="
+echo " Pushing docket images to Docker hub!"
+echo "======================================="
+
+docker push stephengrider/multi-client-k8s:latest
+docker push stephengrider/multi-server-k8s-pgfix:latest
+docker push stephengrider/multi-worker-k8s:latest
+
+docker push stephengrider/multi-client-k8s:$GITHUB_SHA
+docker push stephengrider/multi-server-k8s-pgfix:$GITHUB_SHA
+docker push stephengrider/multi-worker-k8s:$GITHUB_SHA
+
+echo "======================================="
+echo " Apply kubectl deployments!"
+echo "======================================="
 
 kubectl apply -f k8s
-kubectl set image deployments/server-deployment server=pmagas/multi-server-k8s-pgfix:$GITHUB_SHA
-kubectl set image deployments/client-deployment client=pmagas/multi-client-k8s:$GITHUB_SHA
-kubectl set image deployments/worker-deployment worker=pmagas/multi-worker-k8s:$GITHUB_SHA
+
+echo "======================================="
+echo " Set K8s cluster new images!"
+echo "======================================="
+
+kubectl set image deployments/server-deployment server=stephengrider/multi-server-k8s-pgfix:$GITHUB_SHA
+kubectl set image deployments/client-deployment client=stephengrider/multi-client-k8s:$GITHUB_SHA
+kubectl set image deployments/worker-deployment worker=stephengrider/multi-worker-k8s:$GITHUB_SHA
