9b88cf6e6908c912bb4fb655602f0e9a70813a14
Pablo Magro <pmagas@gmail.com>
2023-02-16 13:27:45 +1300

    Add DOCKER_USERNAME script variable.

--- .github/workflows/deploy.sh
+++ .github/workflows/deploy.sh
@@ -4,21 +4,21 @@ echo "======================================="
 echo " Building docket images!"
 echo "======================================="
 
-docker build -t stephengrider/multi-client-k8s:latest -t stephengrider/multi-client-k8s:$GITHUB_SHA -f ./client/Dockerfile ./client
-docker build -t stephengrider/multi-server-k8s-pgfix:latest -t stephengrider/multi-server-k8s-pgfix:$GITHUB_SHA -f ./server/Dockerfile ./server
-docker build -t stephengrider/multi-worker-k8s:latest -t stephengrider/multi-worker-k8s:$GITHUB_SHA -f ./worker/Dockerfile ./worker
+docker build -t $DOCKER_USERNAME/multi-client-k8s:latest -t $DOCKER_USERNAME/multi-client-k8s:$GITHUB_SHA -f ./client/Dockerfile ./client
+docker build -t $DOCKER_USERNAME/multi-server-k8s-pgfix:latest -t $DOCKER_USERNAME/multi-server-k8s-pgfix:$GITHUB_SHA -f ./server/Dockerfile ./server
+docker build -t $DOCKER_USERNAME/multi-worker-k8s:latest -t $DOCKER_USERNAME/multi-worker-k8s:$GITHUB_SHA -f ./worker/Dockerfile ./worker
 
 echo "======================================="
 echo " Pushing docket images to Docker hub!"
 echo "======================================="
 
-docker push stephengrider/multi-client-k8s:latest
-docker push stephengrider/multi-server-k8s-pgfix:latest
-docker push stephengrider/multi-worker-k8s:latest
+docker push $DOCKER_USERNAME/multi-client-k8s:latest
+docker push $DOCKER_USERNAME/multi-server-k8s-pgfix:latest
+docker push $DOCKER_USERNAME/multi-worker-k8s:latest
 
-docker push stephengrider/multi-client-k8s:$GITHUB_SHA
-docker push stephengrider/multi-server-k8s-pgfix:$GITHUB_SHA
-docker push stephengrider/multi-worker-k8s:$GITHUB_SHA
+docker push $DOCKER_USERNAME/multi-client-k8s:$GITHUB_SHA
+docker push $DOCKER_USERNAME/multi-server-k8s-pgfix:$GITHUB_SHA
+docker push $DOCKER_USERNAME/multi-worker-k8s:$GITHUB_SHA
 
 echo "======================================="
 echo " Apply kubectl deployments!"
@@ -30,6 +30,6 @@ echo "======================================="
 echo " Set K8s cluster new images!"
 echo "======================================="
 
-kubectl set image deployments/server-deployment server=stephengrider/multi-server-k8s-pgfix:$GITHUB_SHA
-kubectl set image deployments/client-deployment client=stephengrider/multi-client-k8s:$GITHUB_SHA
-kubectl set image deployments/worker-deployment worker=stephengrider/multi-worker-k8s:$GITHUB_SHA
+kubectl set image deployments/server-deployment server=$DOCKER_USERNAME/multi-server-k8s-pgfix:$GITHUB_SHA
+kubectl set image deployments/client-deployment client=$DOCKER_USERNAME/multi-client-k8s:$GITHUB_SHA
+kubectl set image deployments/worker-deployment worker=$DOCKER_USERNAME/multi-worker-k8s:$GITHUB_SHA
